d23165ff1a5393813481ca57aba1e79c9c3584bb
Cislove <vim152184@gmail.com>
2026-01-10 12:35:59 +0300

    feat: add first version of Stackoverflow client

---
+++ services/StackoverflowService/src/main/kotlin/boysband/linktracker/stackoverflow/dto/Record.kt
@@ -0,0 +1,13 @@
+package boysband.linktracker.boysband.linktracker.stackoverflow.dto
+
+import java.time.ZonedDateTime
+
+data class Record(
+    val author: String,
+    val text: String,
+    val creationDate: ZonedDateTime
+){
+    enum class Type{
+        ANSWER, COMMENT
+    }
+}
